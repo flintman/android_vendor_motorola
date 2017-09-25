Sohamlad7/android_vendor_motorola_cedric

potter: ship N libgui and libui
* Renamed and haxxed to libguy and libuy

potter-vendor.mk
@@ -900,7 +900,10 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/potter/proprietary/lib/libqsap_sdk.so:system/lib/libqsap_sdk.so \
     vendor/motorola/potter/proprietary/bin/mm-qcamera-daemon:system/bin/mm-qcamera-daemon \
     vendor/motorola/potter/proprietary/vendor/lib/libpanelmode.so:system/vendor/lib/libpanelmode.so \
-    vendor/motorola/potter/proprietary/vendor/lib/libbson.so:system/vendor/lib/libbson.so
+    vendor/motorola/potter/proprietary/vendor/lib/libbson.so:system/vendor/lib/libbson.so \
+    vendor/motorola/potter/proprietary/vendor/lib/libguy.so:system/vendor/lib/libguy.so \
+    vendor/motorola/potter/proprietary/vendor/lib/libuy.so:system/vendor/lib/libuy.so \
+
 PRODUCT_PACKAGES += \
     TimeService \
     datastatusnotification \

flintman/android_vendor_motorola

Change-Id: I17def31b8a8354b338a3a61c05655b9858d4383b

potter/potter-vendor.mk
@@ -900,7 +900,10 @@ PRODUCT_COPY_FILES += \
     vendor/moto/potter/proprietary/lib/libqsap_sdk.so:system/lib/libqsap_sdk.so \
     vendor/moto/potter/proprietary/bin/mm-qcamera-daemon:system/bin/mm-qcamera-daemon \
     vendor/moto/potter/proprietary/vendor/lib/libpanelmode.so:system/vendor/lib/libpanelmode.so \
-    vendor/moto/potter/proprietary/vendor/lib/libbson.so:system/vendor/lib/libbson.so
+    vendor/moto/potter/proprietary/vendor/lib/libbson.so:system/vendor/lib/libbson.so \
+    vendor/moto/potter/proprietary/vendor/lib/libguy.so:system/vendor/lib/libguy.so \
+    vendor/moto/potter/proprietary/vendor/lib/libuy.so:system/vendor/lib/libuy.so \
+
 PRODUCT_PACKAGES += \
     TimeService \
     datastatusnotification \