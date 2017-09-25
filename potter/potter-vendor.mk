Sohamlad7/android_vendor_motorola_cedric

potter: ship some camera blobs

potter-vendor.mk
@@ -895,9 +895,12 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/potter/proprietary/vendor/lib/libsdm-disp-apis.so:system/vendor/lib/libsdm-disp-apis.so \
     vendor/motorola/potter/proprietary/vendor/lib/libsdmextension.so:system/vendor/lib/libsdmextension.so \
     vendor/motorola/potter/proprietary/vendor/lib/libsd_sdk_display.so:system/vendor/lib/libsd_sdk_display.so \
+    vendor/motorola/potter/proprietary/lib/libmemalloc.so:system/lib/libmemalloc.so \
     vendor/motorola/potter/proprietary/vendor/lib/libprotobuf-cpp-shit.so:system/vendor/lib/libprotobuf-cpp-shit.so \
-    vendor/motorola/potter/proprietary/lib/libqsap_sdk.so:system/lib/libqsap_sdk.so
-
+    vendor/motorola/potter/proprietary/lib/libqsap_sdk.so:system/lib/libqsap_sdk.so \
+    vendor/motorola/potter/proprietary/bin/mm-qcamera-daemon:system/bin/mm-qcamera-daemon \
+    vendor/motorola/potter/proprietary/vendor/lib/libpanelmode.so:system/vendor/lib/libpanelmode.so \
+    vendor/motorola/potter/proprietary/vendor/lib/libbson.so:system/vendor/lib/libbson.so
 PRODUCT_PACKAGES += \
     TimeService \
     datastatusnotification \

flintman/android_vendor_motorola

Change-Id: I802b19829ad155ad05aa31a8a58d7e4057fa3db9

potter/potter-vendor.mk
@@ -895,9 +895,12 @@ PRODUCT_COPY_FILES += \
     vendor/moto/potter/proprietary/vendor/lib/libsdm-disp-apis.so:system/vendor/lib/libsdm-disp-apis.so \
     vendor/moto/potter/proprietary/vendor/lib/libsdmextension.so:system/vendor/lib/libsdmextension.so \
     vendor/moto/potter/proprietary/vendor/lib/libsd_sdk_display.so:system/vendor/lib/libsd_sdk_display.so \
+    vendor/moto/potter/proprietary/lib/libmemalloc.so:system/lib/libmemalloc.so \
     vendor/moto/potter/proprietary/vendor/lib/libprotobuf-cpp-shit.so:system/vendor/lib/libprotobuf-cpp-shit.so \
-    vendor/moto/potter/proprietary/lib/libqsap_sdk.so:system/lib/libqsap_sdk.so
-
+    vendor/moto/potter/proprietary/lib/libqsap_sdk.so:system/lib/libqsap_sdk.so \
+    vendor/moto/potter/proprietary/bin/mm-qcamera-daemon:system/bin/mm-qcamera-daemon \
+    vendor/moto/potter/proprietary/vendor/lib/libpanelmode.so:system/vendor/lib/libpanelmode.so \
+    vendor/moto/potter/proprietary/vendor/lib/libbson.so:system/vendor/lib/libbson.so
 PRODUCT_PACKAGES += \
     TimeService \
     datastatusnotification \