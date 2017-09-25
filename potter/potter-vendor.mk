Sohamlad7/android_vendor_motorola_cedric

potter: rename sensors HAL for multiHAL

potter-vendor.mk
@@ -141,7 +141,7 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/potter/proprietary/lib/hw/camera.vendor.msm8953.so:system/lib/hw/camera.vendor.msm8953.so \
     vendor/motorola/potter/proprietary/lib/hw/fingerprint.msm8953.so:system/lib/hw/fingerprint.msm8953.so \
     vendor/motorola/potter/proprietary/lib/hw/sensorhub.msm8953.so:system/lib/hw/sensorhub.msm8953.so \
-    vendor/motorola/potter/proprietary/lib/hw/sensors.msm8953.so:system/lib/hw/sensors.msm8953.so \
+    vendor/motorola/potter/proprietary/lib/hw/sensors.msm8953.so:system/lib/sensors.moto.so \
     vendor/motorola/potter/proprietary/lib/lib_fpc_tac_shared.so:system/lib/lib_fpc_tac_shared.so \
     vendor/motorola/potter/proprietary/lib/lib_motsensorlistener.so:system/lib/lib_motsensorlistener.so \
     vendor/motorola/potter/proprietary/lib/lib_mottof.so:system/lib/lib_mottof.so \
@@ -904,7 +904,6 @@ PRODUCT_PACKAGES += \
     TimeService \
     datastatusnotification \
     embms \
-    CNEService \
     colorservice \
     com.qti.location.sdk \
     com.qti.snapdragon.sdk.display \
@@ -913,8 +912,6 @@ PRODUCT_PACKAGES += \
     imscmlibrary \
     qcrilhook \
     com.qualcomm.location \
-    ims \
-    imssettings \
     qcrilmsgtunnel \
     izat.xt.srv \
     qti-telephony-common \

flintman/android_vendor_motorola

Change-Id: I8a54b1233da9adbc474d4c2e8474b5452d0ce003

potter/potter-vendor.mk
@@ -141,7 +141,7 @@ PRODUCT_COPY_FILES += \
     vendor/moto/potter/proprietary/lib/hw/camera.vendor.msm8953.so:system/lib/hw/camera.vendor.msm8953.so \
     vendor/moto/potter/proprietary/lib/hw/fingerprint.msm8953.so:system/lib/hw/fingerprint.msm8953.so \
     vendor/moto/potter/proprietary/lib/hw/sensorhub.msm8953.so:system/lib/hw/sensorhub.msm8953.so \
-    vendor/moto/potter/proprietary/lib/hw/sensors.msm8953.so:system/lib/hw/sensors.msm8953.so \
+    vendor/moto/potter/proprietary/lib/hw/sensors.msm8953.so:system/lib/sensors.moto.so \
     vendor/moto/potter/proprietary/lib/lib_fpc_tac_shared.so:system/lib/lib_fpc_tac_shared.so \
     vendor/moto/potter/proprietary/lib/lib_motsensorlistener.so:system/lib/lib_motsensorlistener.so \
     vendor/moto/potter/proprietary/lib/lib_mottof.so:system/lib/lib_mottof.so \
@@ -904,7 +904,6 @@ PRODUCT_PACKAGES += \
     TimeService \
     datastatusnotification \
     embms \
-    CNEService \
     colorservice \
     com.qti.location.sdk \
     com.qti.snapdragon.sdk.display \
@@ -913,8 +912,6 @@ PRODUCT_PACKAGES += \
     imscmlibrary \
     qcrilhook \
     com.qualcomm.location \
-    ims \
-    imssettings \
     qcrilmsgtunnel \
     izat.xt.srv \
     qti-telephony-common \