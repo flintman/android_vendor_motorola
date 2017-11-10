a64059f32b242683874d8b8e540bf5869a1d923e
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-11-10 16:28:58 -0200

    potter: Add aptX blobs
    Change-Id: I6be390fe2f923bde08d9e86c9cabbe43ba390093

--- potter/potter-vendor.mk
+++ potter/potter-vendor.mk
@@ -138,6 +138,10 @@ PRODUCT_COPY_FILES += \
     vendor/moto/potter/proprietary/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv_India.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv_India.bin \
     vendor/moto/potter/proprietary/etc/firmware/wlan/prima/WCNSS_wlan_dictionary.dat:system/etc/firmware/wlan/prima/WCNSS_wlan_dictionary.dat \
     vendor/moto/potter/proprietary/etc/xtra_root_cert.pem:system/etc/xtra_root_cert.pem \
+    vendor/moto/potter/proprietary/lib/libaptXHD_encoder.so:system/lib/libaptXHD_encoder.so \
+    vendor/moto/potter/proprietary/lib/libaptX_encoder.so:system/lib/libaptX_encoder.so \
+    vendor/moto/potter/proprietary/lib/libldacBT_abr.so:system/lib/libldacBT_abr.so \
+    vendor/moto/potter/proprietary/lib/libldacBT_enc.so:system/lib/libldacBT_enc.so \
     vendor/moto/potter/proprietary/lib/hw/camera.vendor.msm8953.so:system/lib/hw/camera.msm8953.so \
     vendor/moto/potter/proprietary/lib/hw/fingerprint.msm8953.so:system/vendor/lib/hw/fingerprint.vendor.msm8953.so \
     vendor/moto/potter/proprietary/lib/hw/sensorhub.msm8953.so:system/lib/hw/sensorhub.msm8953.so \
