Sohamlad7/android_vendor_motorola_cedric

potter: update adsprpcd

potter-vendor.mk
@@ -15,8 +15,8 @@
 # This file is generated by device/motorola/potter/setup-makefiles.sh
 
 PRODUCT_COPY_FILES += \
-    vendor/motorola/potter/proprietary/bin/adspd:system/bin/adspd \
-    vendor/motorola/potter/proprietary/bin/adsprpcd:system/bin/adsprpcd \
+    vendor/motorola/potter/proprietary/vendor/bin/adspd:system/vendor/bin/adspd \
+    vendor/motorola/potter/proprietary/vendor/bin/adsprpcd:system/vendor/bin/adsprpcd \
     vendor/motorola/potter/proprietary/bin/akmd09912:system/bin/akmd09912 \
     vendor/motorola/potter/proprietary/bin/cnd:system/bin/cnd \
     vendor/motorola/potter/proprietary/vendor/bin/btnvtool:system/vendor/bin/btnvtool \
@@ -250,6 +250,11 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/potter/proprietary/vendor/lib/libadreno_utils.so:system/vendor/lib/libadreno_utils.so \
     vendor/motorola/potter/proprietary/vendor/lib/libadsp_default_listener.so:system/vendor/lib/libadsp_default_listener.so \
     vendor/motorola/potter/proprietary/vendor/lib/libadsprpc.so:system/vendor/lib/libadsprpc.so \
+    vendor/motorola/potter/proprietary/vendor/lib64/libadsp_default_listener.so:system/vendor/lib64/libadsp_default_listener.so \
+    vendor/motorola/potter/proprietary/vendor/lib64/libadsprpc.so:system/vendor/lib64/libadsprpc.so \
+    
+    vendor/motorola/potter/proprietary/vendor/lib64/libadsp_hvx_callback_skel.so:system/vendor/lib64/libadsp_hvx_callback_skel.so \
+    vendor/motorola/potter/proprietary/vendor/lib64/libadsp_hvx_stub.so:system/vendor/lib64/libadsp_hvx_stub.so \
     vendor/motorola/potter/proprietary/vendor/lib/libaptX-1.0.0-rel-Android21-ARMv7A.so:system/vendor/lib/libaptX-1.0.0-rel-Android21-ARMv7A.so \
     vendor/motorola/potter/proprietary/vendor/lib/libaptXScheduler.so:system/vendor/lib/libaptXScheduler.so \
     vendor/motorola/potter/proprietary/vendor/lib/libaudcal.so:system/vendor/lib/libaudcal.so \

flintman/android_vendor_motorola

Change-Id: If0e3037b7e95ac43e61c8b7b02a5617704fe3cea

potter/potter-vendor.mk
@@ -15,8 +15,8 @@
 # This file is generated by device/motorola/potter/setup-makefiles.sh
 
 PRODUCT_COPY_FILES += \
-    vendor/moto/potter/proprietary/bin/adspd:system/bin/adspd \
-    vendor/moto/potter/proprietary/bin/adsprpcd:system/bin/adsprpcd \
+    vendor/moto/potter/proprietary/vendor/bin/adspd:system/vendor/bin/adspd \
+    vendor/moto/potter/proprietary/vendor/bin/adsprpcd:system/vendor/bin/adsprpcd \
     vendor/moto/potter/proprietary/bin/akmd09912:system/bin/akmd09912 \
     vendor/moto/potter/proprietary/bin/cnd:system/bin/cnd \
     vendor/moto/potter/proprietary/vendor/bin/btnvtool:system/vendor/bin/btnvtool \
@@ -250,6 +250,11 @@ PRODUCT_COPY_FILES += \
     vendor/moto/potter/proprietary/vendor/lib/libadreno_utils.so:system/vendor/lib/libadreno_utils.so \
     vendor/moto/potter/proprietary/vendor/lib/libadsp_default_listener.so:system/vendor/lib/libadsp_default_listener.so \
     vendor/moto/potter/proprietary/vendor/lib/libadsprpc.so:system/vendor/lib/libadsprpc.so \
+    vendor/moto/potter/proprietary/vendor/lib64/libadsp_default_listener.so:system/vendor/lib64/libadsp_default_listener.so \
+    vendor/moto/potter/proprietary/vendor/lib64/libadsprpc.so:system/vendor/lib64/libadsprpc.so \
+    
+    vendor/moto/potter/proprietary/vendor/lib64/libadsp_hvx_callback_skel.so:system/vendor/lib64/libadsp_hvx_callback_skel.so \
+    vendor/moto/potter/proprietary/vendor/lib64/libadsp_hvx_stub.so:system/vendor/lib64/libadsp_hvx_stub.so \
     vendor/moto/potter/proprietary/vendor/lib/libaptX-1.0.0-rel-Android21-ARMv7A.so:system/vendor/lib/libaptX-1.0.0-rel-Android21-ARMv7A.so \
     vendor/moto/potter/proprietary/vendor/lib/libaptXScheduler.so:system/vendor/lib/libaptXScheduler.so \
     vendor/moto/potter/proprietary/vendor/lib/libaudcal.so:system/vendor/lib/libaudcal.so \