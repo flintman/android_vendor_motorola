6b5ba685fb3f1ea6c9d2e8048a0f01153c27fc0c
camcory <camcory1@gmail.com>
2017-12-19 16:46:16 -0500

    Remove libfmas remnants
    -edited from original by moepda
    
    Change-Id: Id798ad3c862ad8181738533fcd835b0178d2fb83

--- shamu/shamu-vendor-blobs.mk
+++ shamu/shamu-vendor-blobs.mk
@@ -94,7 +94,6 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/shamu/proprietary/lib/hw/audio.motvr.default.so:system/lib/hw/audio.motvr.default.so \
     vendor/motorola/shamu/proprietary/lib/hw/audio.primary.msm8084.so:system/lib/hw/audio.primary.msm8084.so \
     vendor/motorola/shamu/proprietary/lib/hw/sound_trigger.primary.shamu.so:system/lib/hw/sound_trigger.primary.shamu.so \
-    vendor/motorola/shamu/proprietary/vendor/lib/soundfx/libfmas.so:system/lib/soundfx/libfmas.so \
     vendor/motorola/shamu/proprietary/vendor/lib/soundfx/libqcomvisualizer.so:system/lib/soundfx/libqcomvisualizer.so \
     vendor/motorola/shamu/proprietary/vendor/lib/soundfx/libqcomvoiceprocessing.so:system/lib/soundfx/libqcomvoiceprocessing.so \
     vendor/motorola/shamu/proprietary/vendor/lib/soundfx/libspeakerbundle.so:system/lib/soundfx/libspeakerbundle.so \
