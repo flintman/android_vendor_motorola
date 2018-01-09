a59bf3b7f5c3adf2f1c7957b3dec6405d880190a
Vachounet <vachounet@live.fr>
2018-01-09 11:13:37 +0100

    potter: add back CNEService again
    * Kanged from op3
    * Add keystore HAL from tissot
    
    Change-Id: Ie0d97af54b5810f42bf8f436550cf20d876647bb

--- potter/potter-vendor.mk
+++ potter/potter-vendor.mk
@@ -199,6 +199,7 @@ PRODUCT_COPY_FILES += \
     vendor/moto/potter/proprietary/vendor/lib/hw/flp.default.so:system/vendor/lib/hw/flp.default.so \
     vendor/moto/potter/proprietary/vendor/lib/hw/gatekeeper.msm8953.so:system/vendor/lib/hw/gatekeeper.msm8953.so \
     vendor/moto/potter/proprietary/vendor/lib/hw/keystore.msm8953.so:system/vendor/lib/hw/keystore.msm8953.so \
+    vendor/moto/potter/proprietary/vendor/lib64/hw/keystore.msm8953.so:system/vendor/lib64/hw/keystore.msm8953.so \
     vendor/moto/potter/proprietary/vendor/lib/hw/vulkan.msm8953.so:system/vendor/lib/hw/vulkan.msm8953.so \
     vendor/moto/potter/proprietary/vendor/lib/lib-dplmedia.so:system/vendor/lib/lib-dplmedia.so \
     vendor/moto/potter/proprietary/vendor/lib/lib-ims-rcscmjni.so:system/vendor/lib/lib-ims-rcscmjni.so \
@@ -1226,5 +1227,6 @@ PRODUCT_PACKAGES += \
     HotwordEnrollmentOKGoogleCS47L35 \
     HotwordEnrollmentTGoogleCS47L35 \
     HotwordEnrollmentXGoogleCS47L35 \
-    QtiTelephonyService
+    QtiTelephonyService \
+    CNEService
 
