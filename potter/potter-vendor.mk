elginsk8r/android_vendor_moto

potter: Move WfdCommon to soong
Change-Id: I66d138e6f8aa04bd1823050e041f8ec9c58457ba

potter/potter-vendor.mk
@@ -1398,3 +1398,5 @@ PRODUCT_PACKAGES += \
     libbtnv \
     WfdService \
     WfdCommon
+
+PRODUCT_SOONG_NAMESPACES += vendor/moto/potter

flintman/android_vendor_motorola

potter/potter-vendor.mk
@@ -1350,3 +1350,5 @@ PRODUCT_PACKAGES += \
     libbtnv \
     WfdService \
     WfdCommon
+
+PRODUCT_SOONG_NAMESPACES += vendor/motorola/potter